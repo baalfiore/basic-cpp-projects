79ea5f3f73c6a3d7e4e25e1236fde0b38077a710
malfyore <brandedreal@gmail.com>
2021-08-03 19:22:37 -0400

    Cleaned up code, used arrays with pointers for fun

--- 03_turtles/main.cpp
+++ 03_turtles/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 /**
@@ -78,20 +79,31 @@ int main()
     Turtle *tptr = NULL;
     Sponsor *sptr = NULL;
 
-    sptr = &disney;
-    setSponsor(*sptr, "Disney Conservation Fund", "1995");
-    sptr = &wildlife;
-    setSponsor(wildlife, "Wildlife Collections", "2018");
-    sptr = &tarte;
-    setSponsor(tarte, "Tarte Cosmetics", "1999");
-    tptr = &poptart;
-    setTurtle(poptart, "Poptart", "Female", 110.2, 97.1, "Loggerhead", wildlife);
-    tptr = &raya;
-    setTurtle(raya, "Raya", "Female", 87.1, 83.7, "Loggerhead", disney);
-    tptr = &coral;
-    setTurtle(coral, "Coral", "Female", 97.5, 91.0, "Loggerhead/HawksBill", tarte);
+    // Arrays
+    Turtle turtles[] = { poptart, raya, coral};
+    Sponsor sponsors[] = { wildlife, disney, tarte};
+   
+    int arrSize =  (sizeof(turtles)/sizeof(turtles[0]));
+    int i = 0;
 
-    printTurtle(poptart);
-    printTurtle(raya);
-    printTurtle(coral);
+    setSponsor(sponsors[0], "Wildlife Collections", "2018");
+    setSponsor(sponsors[1], "Disney Conservation Fund", "1995");
+    setSponsor(sponsors[2], "Tarte Cosmetics", "1999");
+    
+    setTurtle(turtles[0], "Poptart", "Female", 110.2, 97.1, "Loggerhead", sponsors[1]);
+    setTurtle(turtles[1], "Raya", "Female", 87.1, 83.7, "Loggerhead", sponsors[0]);
+    setTurtle(turtles[2], "Coral", "Female", 97.5, 91.0, "Loggerhead/HawksBill", sponsors[2]);
+
+    sptr = sponsors;
+    tptr = turtles;
+
+    do {
+        cout << "tptr @: " << tptr << endl;
+        printTurtle(*tptr);
+        printSponsor(*sptr);
+        tptr+=1;
+        sptr+=1;
+        i+=1;
+    } while(i!=arrSize);
+
 };

e34043bebc0ff0bad769f97c2720ff24d6082689
malfyore <brandedreal@gmail.com>
2021-08-04 18:10:48 -0400

    Added scoring system.

--- 04_quiz/main.cpp
+++ 04_quiz/main.cpp
@@ -54,45 +54,53 @@ void setResponse( Response& _response, string _text, bool Correct)
 }
 
 //validates the response of the user for a correct answer.
-bool isCorrect(Question& _question, Response choice)
+bool isCorrect(Question& _question, Response choice, int& score)
 {
   
     if (_question.answer != choice.text)
     {
         cout << "Incorrect." << endl;
+       
         return false;
     }
     else 
     {
-        cout << "You are correct!" << endl;
+        cout << "You are correct! " << endl;
+         score+=1;
         return true;
     }
 }
 
 void displayError()
 {
-    cout << "Error: Try again" << endl;
+    cout << "An Error has occurred. " << endl;
 }
 
-void displayQuestion(Question& _question, Response _response[],int arrSize)
+void displayScore(int& score)
+{
+    cout << "----- Current score: " << score << endl;
+}
+
+void displayQuestion(Question& _question, Response _response[], int arrSize, int& _score)
 {   
+    displayScore(_score);
     cout << _question.prompt << "?" << endl;
     for(int x =0; x != arrSize; ++x)
     {
         cout << "   " << x+1 << ". " << _response[x].text << endl;
     } 
 
-    cout << "Enter the number of your choice (1 -" << arrSize << endl;
+    cout << "Enter the number of your choice (1 -" << arrSize << ")" << endl;
     int choice;
     cin >> choice;
-    cout << "\nYou have selected: " << choice-1 << endl;
+    cout << "\nYou have selected: " << choice << endl;
     if (isValidChoice(choice -1, arrSize)!= false)
     {
-        isCorrect(_question, _response[choice-1]);
+        isCorrect(_question, _response[choice-1], _score);
     }
     else 
     {
-
+        displayError();
     }
     
 }
@@ -105,19 +113,24 @@ int main()
     Response r1, r2, r3;
     Response responses[] = {r1, r2, r3};
 
+    int score = 0;
+    int* scorePtr = NULL;
+
+    scorePtr = &score;
+
     int arrSize = sizeof(responses)/sizeof(responses[0]);
 
     setResponse(responses[0], "periods", false);
     setResponse(responses[1], "commas", true);
     setResponse(responses[2], "dashes", false);
-    setQuestion(*qptr, "What do Loggerhead Tracks look like?", "commas", responses, arrSize );
-    displayQuestion(*qptr, responses, arrSize);
+    setQuestion(*qptr, "What do Loggerhead tracks look like?", "commas", responses, arrSize );
+    displayQuestion(*qptr, responses, arrSize, *scorePtr);
 
     setResponse(responses[0], "in the ocean", false);
     setResponse(responses[1], "on grassy soil", false);
     setResponse(responses[2], "on shore", true);
     setQuestion(*qptr, "Where do female turtle typically lay their eggs at?", "on shore", responses, arrSize );
-    displayQuestion(*qptr, responses, arrSize);
+    displayQuestion(*qptr, responses, arrSize, *scorePtr);
 
 }
 
